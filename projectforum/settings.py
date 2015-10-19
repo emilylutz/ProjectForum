7f6fbbf3509898dded89557ef52deaaf84fdf9fc
ucarunke <arunke@berkeley.edu>
2015-10-18 18:08:07 -0700

    Turn debug on

--- projectforum/settings.py
+++ projectforum/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z-k3y_em5)2=gdh=cnry4ltlue=w(hem6&@34@+i)rsnbhwbxa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
