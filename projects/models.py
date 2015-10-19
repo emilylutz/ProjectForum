64b225fc937af5d3b2527b4dbd244cabff5036bd
Emily Lutz <emily.lutz@berkeley.edu>
2015-10-18 19:02:44 -0700

    added basic Project model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+#title, description, owner, team, status, tags
+class Project(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=2048)
+    #owner = models.ForeignKey(User)
+    #team = models.ManyToManyField(User)
+    #status = models.ForeignKey(User)
+    #tags = models.ManyToManyField(charField, max_length=128)
+    #timestamp = models.DateTimeField()
+    
