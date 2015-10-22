b21fa9c8ecb084788e5c6cf0692116fc4bc5862d
ucarunke <arunke@berkeley.edu>
2015-10-22 13:07:47 -0700

    Profiles

--- projectforum/user_profiles/forms.py
+++ projectforum/user_profiles/forms.py
@@ -12,3 +12,13 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = (getattr(model, 'USERNAME_FIELD', 'username'), 'email')
+
+    def clean_email(self):
+        """
+        Validate that the supplied email address is unique for the
+        site.
+        """
+        model = get_user_model()
+        if model.objects.filter(email__iexact=self.cleaned_data['email']):
+            raise forms.ValidationError("This email address is already in use.")
+        return self.cleaned_data['email']
