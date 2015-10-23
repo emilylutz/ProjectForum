c03d27ee203eee2b4eaa73f0a5808eceae497bec
Emily Lutz <emily.lutz@berkeley.edu>
2015-10-22 17:45:05 -0700

    test for creating and getting projects

--- projectforum/projects/tests.py
+++ projectforum/projects/tests.py
@@ -1,10 +1,24 @@
 from django.test import TestCase
+from projectforum.projects.models import Project
 
 class ProjectsTest(TestCase):
-	def setUp(self):
-		print "setting up"
+	#def setUp(self):
+		# add any set-up code here
 
 	def test_that_tests_work(self):
 		print "test works"
+
+	def test_projects_can_be_created(self):
+		project1 = Project.objects.create(
+			title = "Test Title",
+			description = "Test Description",
+		)
+		self.assertEqual(project1.title, "Test Title")
+		self.assertEqual(project1.description, "Test Description")
+		print "projects can be created"
+
+		fetched_projects = Project.objects.filter()
+		self.assertEqual(1, len(fetched_projects))
+		print "projects can be gotten"
 
 # Create your tests here.
