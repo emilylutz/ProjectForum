11d26af9cafd80ad3849834995fbf8f7d00ce2de
Sarah Au <sau@berkeley.edu>
2015-10-22 23:43:35 -0700

    added project creation

--- projectforum/projects/models.py
+++ projectforum/projects/models.py
@@ -1,12 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 #title, description, owner, team, status, tags
 class Project(models.Model):
+
+    PAYMENT_CHOICES = (
+        (1, "Lump sum"),
+        (2, "Hourly"),
+    )
+
+    STATUSES = (
+        (1, "In progress"),
+        (2, 'Canceled'),
+        (3, 'Finished'),
+    )
+
     title = models.CharField(max_length=128)
     description = models.CharField(max_length=2048)
-    #owner = models.ForeignKey(User)
+    owner = models.ForeignKey(User)
     #team = models.ManyToManyField(User)
-    #status = models.ForeignKey(User)
-    #tags = models.ManyToManyField(charField, max_length=128)
-    #timestamp = models.DateTimeField()
-    
+    payment = models.IntegerField(choices=PAYMENT_CHOICES)
+    amount = models.IntegerField()
+    status = models.IntegerField(choices=STATUSES, default=1)
+    tags = models.CharField(max_length=2048, blank=True)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
--- projectforum/projects/urls.py
+++ projectforum/projects/urls.py
@@ -3,11 +3,20 @@
 from django.conf.urls import include, patterns, url
 from django.views.generic import TemplateView
 
-from .views import ProjectListView
+from .views import *
 
 urlpatterns = [
     url(r'^list/$',
         ProjectListView.as_view(),
         name='list'),
+    url(r'^create/$',
+        CreateView.as_view(),
+        name='create'),
+    url(r'^create/complete/$',
+        TemplateView.as_view(template_name='create_success.html'),
+        name='create_success'),
+    url(r'^create/fail/$',
+        TemplateView.as_view(template_name='create_fail.html'),
+        name='create_fail'),
     #add more urls here
 ]
