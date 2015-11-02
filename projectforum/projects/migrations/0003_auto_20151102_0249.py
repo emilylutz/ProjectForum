43a58ce032fea057e6f67dd30fb562aca0db6655
Cedric Lamy <lamycedric@gmail.com>
2015-11-01 19:10:59 -0800

    Fixed small typo dependency stuffs

--- projectforum/projects/migrations/0003_auto_20151102_0249.py
+++ projectforum/projects/migrations/0003_auto_20151102_0249.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('projects', '0001_initial'),
+        ('projects', '0002_auto_20151022_2201'),
     ]
 
     operations = [
