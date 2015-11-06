d8dd6c653286d284820a0d36134de72bb070a9ed
Cedric Lamy <lamycedric@gmail.com>
2015-11-05 18:36:47 -0800

    build first test

--- projectforum/projects/tests.py
+++ projectforum/projects/tests.py
@@ -165,6 +165,6 @@ class ProjectsTest(TestCase):
         test_create_projects.create_many_projects()
         response = project_filters.get_project_list(order='title', salary='Lump Sum',
                             ascending=False, starting_from = 0, ending_at = 10)
-        contents = json.loads(response)
+        contents = json.loads(response.content)
         self.assertEqual(1, contents.status)
         self.assertEqual('F', contents.projects[0].title)
