6424a8a0505be6b3fbda4c740f7f02802d2798b5
Sarah Au <sau@berkeley.edu>
2015-11-05 18:41:49 -0800

    error checking updated + extra files and lines deleted

--- projectforum/ratings/urls.py
+++
@@ -1,9 +0,0 @@
-""" ProjectForum's project module URL Configuration """
-
-from django.conf.urls import include, patterns, url
-
-from .views import *
-
-urlpatterns = [
-    url(r'^review/$', views.make_review)
-]
