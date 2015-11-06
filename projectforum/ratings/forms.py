2a5548a1e8e2e94f5238c46560bb8eaf0abdcbad
Sarah Au <sau@berkeley.edu>
2015-11-05 16:33:20 -0800

    rating form changed

--- projectforum/ratings/forms.py
+++ projectforum/ratings/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from projectforum.ratings.models import *
 
 class RatingForm(forms.ModelForm):
+    comment = forms.CharField(widget=forms.Textarea(attrs={'rows':'3', 'cols': '40'}))
+
     class Meta:
         model = UserReview
         fields = ('score', 'comment')
