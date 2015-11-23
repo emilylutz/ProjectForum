08d14cd87478951e22de1216948377b79d258d7a
ucarunke <arunke@berkeley.edu>
2015-11-22 17:24:02 -0800

    Clean project description field

--- projectforum/projects/forms.py
+++ projectforum/projects/forms.py
@@ -20,7 +20,7 @@ class ProjectForm(forms.ModelForm):
     def clean_title(self):
         """
         Validate that the title has more than whitespace. Strip the whitespace
-        at the start and end.
+        at the start and end. Remove line breaks.
         """
         new_title = self.cleaned_data['title'].lstrip().rstrip()
         new_title = new_title.replace('\n', ' ').replace('\r', '')
@@ -28,3 +28,14 @@ class ProjectForm(forms.ModelForm):
             raise forms.ValidationError("Please use a title with more than "
                                         "just whitespace.")
         return new_title
+
+    def clean_description(self):
+        """
+        Validate that the title has more than whitespace. Strip the whitespace
+        at the start and end.
+        """
+        new_description = self.cleaned_data['description'].lstrip().rstrip()
+        if len(new_description) <= 0:
+            raise forms.ValidationError("Please use a description with more "
+                                        "than just whitespace.")
+        return new_description
