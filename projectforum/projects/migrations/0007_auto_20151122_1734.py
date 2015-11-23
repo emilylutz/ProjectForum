b513a701106a2aa1351063c010086070a336b58f
Sarah Au <sau@berkeley.edu>
2015-11-22 17:36:26 -0800

    amount validation in model

---
+++ projectforum/projects/migrations/0007_auto_20151122_1734.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.core.validators
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('projects', '0006_auto_20151121_0206'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='project',
+            name='amount',
+            field=models.IntegerField(validators=[django.core.validators.MinValueValidator(0, message=b'Please enter a positive amount')]),
+        ),
+        migrations.AlterField(
+            model_name='project',
+            name='description',
+            field=models.TextField(max_length=2048),
+        ),
+    ]
