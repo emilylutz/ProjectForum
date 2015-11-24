0c9fddf2e0991490a38941c7b115e9bb1b02249f
Emily Lutz <emily.lutz@berkeley.edu>
2015-11-23 20:05:28 -0800

    stubbing out things to test

--- projectforum/projects/tests/test_projects_selenium.py
+++ projectforum/projects/tests/test_projects_selenium.py
@@ -19,3 +19,29 @@ class ProjectsSeleniumTest(SeleniumTestCase):
     def _test_that_selenium_index_loads(self):
         print('3')
         self.open(reverse('index'))
+
+    @wrap_with_drivers()
+    def _test_header_links_work(self):
+        #home, about, projects, view all projects, create new project, about
+        #login
+        #then once logged in:
+        #username, view profile, change password, log out
+        pass
+
+    @wrap_with_drivers()
+    def _test_that_user_can_view_list_of_projects(self):
+        pass
+
+    @wrap_with_drivers()
+    def _test_that_user_can_log_in(self):
+        pass
+
+    @wrap_with_drivers()
+    def _test_that_user_can_log_out(self):
+        pass
+
+    @wrap_with_drivers()
+    def _test_that_user_can_change_password(self):
+        pass
+
+
