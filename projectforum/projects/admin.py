33b2b148b2945c8e79cd88468f13ae1c44909be0
Emily Lutz <emily.lutz@berkeley.edu>
2015-11-23 17:38:54 -0800

    added projectapplications to admin panel

--- projectforum/projects/admin.py
+++ projectforum/projects/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from projectforum.projects.models import Project, ProjectTag
+from projectforum.projects.models import Project, ProjectTag, ProjectApplication
 
 
 class ProjectAdmin(admin.ModelAdmin):
@@ -12,5 +12,9 @@ admin.site.register(Project, ProjectAdmin)
 class ProjectTagAdmin(admin.ModelAdmin):
     pass
 
-
 admin.site.register(ProjectTag, ProjectTagAdmin)
+
+class ProjectApplicationAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(ProjectApplication, ProjectApplicationAdmin)
