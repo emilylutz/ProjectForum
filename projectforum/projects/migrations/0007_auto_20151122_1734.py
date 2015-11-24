4f96670af765494864516ea38c72643f51fd2337
Sarah Au <sau@berkeley.edu>
2015-11-23 19:58:25 -0800

    user profile not saving

---
+++ projectforum/projects/migrations/0007_auto_20151122_1734.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.core.validators
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('projects', '0006_auto_20151122_2356'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='project',
+            name='amount',
+            field=models.IntegerField(validators=[django.core.validators.MinValueValidator(0, message=b'Please enter a positive amount')]),
+        ),
+    ]
